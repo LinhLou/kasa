88e6daa8ed1f25b36dcd65788cd9f1f35a46af18
linh lou <dang.tn.linh@gmail.com>
2023-10-13 17:08:40 +0200

    interface

--- src/pages/Propos/Propos.jsx
+++ src/pages/Propos/Propos.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import HeaderMain from '../../components/HeaderMain';
+import ProposMainStyles from './styles/Propos.styled';
+import Dropdown from '../../components/Dropdown';
+import photo from '../../assets/Image_source_2.png';
 
 export default function Propos() {
+  const weightDropdown = '1023px';
   return (
-    <div>propos</div>
+    <ProposMainStyles>
+      <HeaderMain photo={photo} h='223px'/>
+      <div id="propos">
+        <Dropdown w={weightDropdown} title='Fiabilité' infos={['test 111']}/>
+        <Dropdown w={weightDropdown} title='Respect' infos={['test 112']}/>
+        <Dropdown w={weightDropdown} title='Service' infos={['test 113']}/>
+        <Dropdown w={weightDropdown} title='Sécurité' infos={['test 114']}/>
+      </div>
+    </ProposMainStyles>
   )
 }
---
+++ src/pages/Propos/styles/Propos.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const ProposMainStyles = styled.main`
+
+  #propos {
+    margin-top:38px;
+    margin-bottom: 38px;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap:38px;
+  }
+
+`
+
+export default ProposMainStyles
